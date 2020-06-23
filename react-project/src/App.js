cc6bfdc62747faf64cb0fcfabd36e33182e599b2
Arvin Alhissou <arvin.alhissou@gmail.com>
2020-06-23 22:42:11 +0300

    add our components

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import MyNavbar from "/components/navbar";
+import MyNavbar from "./components/navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Myfooter from "./components/footer";
 
 function App() {
   return (
-    <>
-      <MyNavbar />;
-      <Myfooter />;
-    </>
+    <React.Fragment>
+      <div className="page-container">
+        <div className="content-wrap">
+          <MyNavbar />;
+          
+        </div>
+        <Myfooter />;
+      </div>
+    </React.Fragment>
   );
 }
 
