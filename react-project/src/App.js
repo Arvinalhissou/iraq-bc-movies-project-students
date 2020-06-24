7b65185327cdca2776bb2909f0bc92ffda1cb24b
Neven bedr <neven2225@gmail.com>
2020-06-24 14:49:18 +0300

    Changing navbar

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import MyNavbar from "/components/navbar";
+import MyNavbar from "./components/navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Myfooter from "./components/footer";
 
--- react-project/src/components/navbar.js
+++ react-project/src/components/navbar.js
@@ -1,36 +1,25 @@
 import React, { Component } from "react";
+import { MdMovieFilter } from "react-icons/md";
 
-import {
-  Container,
-  Row,
-  Col,
-  Navbar,
-  Nav,
-  FormControl,
-  Form,
-  Button,
-  InputGroup,
-} from "react-bootstrap";
-
-export default function MyNavbar() {
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+export default function myNavbar() {
   return (
-    <Navbar className="bg-light justify-content-between">
-      <Form inline>
-        <InputGroup>
-          <InputGroup.Prepend>
-            <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
-          </InputGroup.Prepend>
-          <FormControl
-            placeholder="Username"
-            aria-label="Username"
-            aria-describedby="basic-addon1"
-          />
-        </InputGroup>
-      </Form>
-      <Form inline>
-        <FormControl type="text" placeholder="Search" className=" mr-sm-2" />
-        <Button type="submit">Submit</Button>
-      </Form>
-    </Navbar>
+    <>
+      <Navbar bg="light" variant="light">
+        <Navbar.Brand href="#home">
+          <MdMovieFilter size="2.5rem" color="#17a2b8" />
+        </Navbar.Brand>
+        <Nav className="ml-auto">
+          {/* <FaHome size="1.2rem" className="mr-2" /> */}
+          <Nav.Link href="#home" className="mr-5">
+            HOME
+          </Nav.Link>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-info">Search</Button>
+          </Form>
+        </Nav>
+      </Navbar>
+    </>
   );
 }
